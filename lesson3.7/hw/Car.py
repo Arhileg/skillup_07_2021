c6b32f876bdc2d9d7fe3754d7d56b49758270c65
Oleg Bulihyn <olegdiablo@gmail.com>
2021-07-15 11:42:23 +0300

    Class Car , add docsring

--- lesson3.7/hw/Car.py
+++ lesson3.7/hw/Car.py
@@ -1,13 +1,35 @@
 class Car:
+    """Car class.
+    Extended description of generic_socket.
+    Parameters
+    ----------
+    model : str
+        Name of car
+    brend : str
+        Manufacturer
+    year : int
+        Year creation
+    engine_volume : int
+        engine volume in cm3
+    color : str
+        Color car
+    price : float
+        Price selling
+    -------
+    int
+        Description of return value
+    """
     def __init__(self, **kwargs):
+        """Заполнение атрибутов, если **kwargs пустой, то по умолчанию."""
         self.model = kwargs.get('model', None)
-        self.manufacturer = kwargs.get('manufacturer', None)
+        self.brend = kwargs.get('brend', None)
         self.year = kwargs.get('year', None)
-        self.engine_volume = kwargs.get('engine_volume', None)
+        self.engine_volume = kwargs.get('engine_volume', 0)
         self.color = kwargs.get('color', None)
         self.price = kwargs.get('price', 0.0)
 
     def __str__(self) -> str:
+        """ДЛя функции print()."""
         result = ""
         for key, value in self.__dict__.items():
             result += f"{key}: {value}\n"
@@ -16,8 +38,8 @@ class Car:
     def get_model(self):
         return self.model
 
-    def get_manufacturer(self):
-        return self.manufacturer
+    def get_brend(self):
+        return self.brend
 
     def get_year(self):
         return self.year
@@ -34,8 +56,8 @@ class Car:
     def set_model(self, model):
         self.model = model
 
-    def set_manufacturer(self, manufacturer):
-        self.manufacturer = manufacturer
+    def set_brend(self, brend):
+        self.brend = brend
 
     def set_year(self, year):
         self.year = year
@@ -53,7 +75,7 @@ class Car:
 if __name__=="__main__":
     bmw = Car(
             model="bmw m3", 
-            manufacturer="bmw", 
+            brend="bmw", 
             year=2000, 
             engine_volume=2993, 
             color='black',
---
+++ lesson3.7/hw/docs_classes.py
@@ -0,0 +1,3 @@
+import Car
+
+print(help(Car))
